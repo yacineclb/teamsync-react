8602ad5d75021c5389eaa8372f54fb2f6b97ada6
yacineclb <cyapro.development@gmail.com>
2024-04-04 17:25:56 +0200

    add tailwindcss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
 }
