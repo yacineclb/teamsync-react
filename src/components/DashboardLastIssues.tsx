4703fa1c9563d580f107b3d72f590afac398b7ea
yacineclb <cyapro.development@gmail.com>
2024-04-05 14:17:54 +0200

    add DashboardLastIssues component

---
+++ src/components/DashboardLastIssues.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardCard from './DashboardCard';
+
+export default function DashboardLastIssues() {
+  return (
+    <DashboardCard>
+      <DashboardCard.Title>Last issues created</DashboardCard.Title>
+      <DashboardCard.Body>This is a beautful card</DashboardCard.Body>
+    </DashboardCard>
+  );
+}
