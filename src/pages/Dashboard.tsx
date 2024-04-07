91b12575c30a0bbe93c97c794572bda0f1ca98d1
yacineclb <cyapro.development@gmail.com>
2024-04-07 22:50:18 +0200

    add dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,37 +1,21 @@
-import DashboardCard from '../components/DashboardCard';
 import DashboardIssues from '../components/DashboardIssues';
 import DashboardLastIssues from '../components/DashboardLastIssues';
-import useUsers from '../hooks/useUsers';
-import { User } from '../api/types';
+import useIssueQuery from '../hooks/useIssueQuery';
 
 export default function Dashboard() {
-  const {users, loading, error} = useUsers()
+  const { issues, loading: loadingQuery } = useIssueQuery();
 
   return (
     <div className="w-full h-full p-12">
       <main className="">
-        <h1 className="text-3xl font-bold underline">Dashboard</h1>
-        {loading && <p>Loading...</p>}
-        {users.map((u: User) => {
-          return (
-            <div key={u.id}>
-              <p>{u.name}</p>
-            </div>
-          );
-        })}
-        {error && <p>{error}</p>}
-        <section className="grid grid-cols-2 gap-6">
-          <DashboardIssues />
-          <DashboardLastIssues />
-          <DashboardCard>
-            <DashboardCard.Title>Project progress</DashboardCard.Title>
-            <DashboardCard.Body>This is a beautful card</DashboardCard.Body>
-          </DashboardCard>
-          <DashboardCard>
-            <DashboardCard.Title>Issues Killer</DashboardCard.Title>
-            <DashboardCard.Body>This is a beautful card</DashboardCard.Body>
-          </DashboardCard>
-        </section>
+        <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+        {loadingQuery && <p>Loading...</p>}
+        {!loadingQuery && (
+          <section className="grid grid-cols-2 gap-6">
+            <DashboardIssues issues={issues} />
+            <DashboardLastIssues issues={issues.slice(0, 5)} />
+          </section>
+        )}
       </main>
     </div>
   );
